2ad5464ab4647cfe6041f911996e545b4f7865e9
Arun Mohan Sharma <arunm2470@gmail.com>
2020-07-08 07:20:42 +0530

    Day 7

---
+++ Day7.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        
+        int count = 0;
+        
+        for(int i = 0; i < grid.size(); i++)
+        {
+            for(int j = 0; j < grid[i].size(); j++)
+            {
+                
+                if(grid[i][j] == 1)
+                {
+                   
+                    if(i > 0)
+                    {
+                       if(grid[i-1][j] == 0)
+                           count++;
+                    }
+                    else
+                        count++;
+
+                    if(i < grid.size()-1)
+                    {
+                         if(grid[i+1][j] == 0)
+                           count++;
+                    }
+                    else
+                        count++;
+
+                    if(j > 0)
+                    {
+                        if(grid[i][j-1] == 0)
+                           count++;
+                    }
+                    else
+                        count++;
+
+                    if(j < grid[i].size()-1)
+                    {
+                        if(grid[i][j+1] == 0)
+                           count++;
+                    }
+                    else
+                        count++;
+                }
+            }
+        }
+        
+        return count;
+        
+    }
+};
